66a3f120e1773fce3993cab847f24002cfc0d4df
Olivier Louvignes <olivier@mg-crea.com>
2014-06-16 13:41:50 +0200

    test(cleancss): add unit tests

--- src/index.js
+++ src/index.js
@@ -12,15 +12,25 @@ var CleanCSS = require('clean-css');
 module.exports = function(options) {
   options = options || {};
 
+  var cleancss = new CleanCSS(options);
+
   function transform(file, encoding, next) {
 
     if(file.isNull()) {
       return next(null, file); // pass along
     }
 
-    file.contents = new Buffer(new CleanCSS(options).minify(file.contents));
-    file.path = gutil.replaceExtension(file.path, '.css');
-    if(options.debug) gutil.log(util.format('File \'%s\' created.', chalk.cyan(path.relative(process.cwd(), file.path))));
+    try {
+      file.contents = new Buffer(cleancss.minify(file.contents));
+      file.path = gutil.replaceExtension(file.path, '.css');
+    } catch(err) {
+      return next(new gutil.PluginError('gulp-cleancss', err));
+    }
+
+    if(options.debug) {
+      gutil.log(util.format('File \'%s\' created.', chalk.cyan(path.relative(process.cwd(), file.path))));
+    }
+
     next(null, file);
 
   }
